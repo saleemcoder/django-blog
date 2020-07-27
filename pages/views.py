5309eb0cc5e87c226040482763eca7743b704354
saleemcoder <saleem.coder@gmail.com>
2020-07-27 09:50:38 +0500

    all static assists are set and ready for db

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
     return render(request, 'pages/index.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def contact(request):
+    return render(request, 'pages/contact-us.html')
+
+def blogs(request):
+    return render(request, 'pages/blogs.html')
